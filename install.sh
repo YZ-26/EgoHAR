ec435f71f1e1c46d686f013988421c37cb2e2ae8
Yerassyl Zhalgasbayev <122618521+YZ-26@users.noreply.github.com>
2025-07-22 06:06:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,11 +10,12 @@ rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
 
 # Create & activate the Conda environment
-~/miniconda3/bin/conda create -n egtea-env python==3.11 -y
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate egtea-env
 conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
 conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+
+conda create -n egtea-env python==3.11 -y
+conda activate egtea-env
 
 # Install core requirements
 pip install -r requirements.txt
@@ -45,16 +46,16 @@ python main.py --trimmed_actions --out /workspace/data
 
 rm -rf /workspace/EGTEA_Gaze_Plus_Downloader
 
+cd /workspace/data/EGTEA/Trimmed_Action_Clips
+tar -xvf video_clips.tar
+
 # Preprocess the data
 cd /workspace/EgoHAR
-python preprocess.py
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate egtea-env
+python Preprocessing.py
 
 # Move files to the correct location
 mv /workspace/EgoHAR/Gaze_Data /workspace/
 mv /workspace/EgoHAR/train_split1_filtered.txt /workspace/
 mv /workspace/EgoHAR/test_split1_filtered.txt /workspace/
-
-
-
-
-
